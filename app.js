5a994e77cf5c33372afeb4545951384f06de6efc
LAI <77723979+LAI-Recycle@users.noreply.github.com>
2023-08-23 21:35:03 +0800

    project init

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
 const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
