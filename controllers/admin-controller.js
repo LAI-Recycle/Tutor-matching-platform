0d9a8477ba7ddc46ef021a516707d3c8a4e1ead7
LAI <77723979+LAI-Recycle@users.noreply.github.com>
2023-08-25 01:17:51 +0800

    feat: create tutor

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -7,6 +7,25 @@ const adminController = {
     })
     .then(tutors => res.render('admin/tutors', { tutors }))
     .catch(err => next(err))
+  },
+  createTutor: (req, res) => {    
+    return res.render('admin/create-tutor')  
+  },
+  postTutor: (req, res, next) => {
+    
+    const { name, tel, courseDescription, teachingStyle } = req.body  // 從 req.body 拿出表單裡的資料
+    if (!name) throw new Error('Tutor name is required!') 
+    Tutor.create({ //產生一個新的 Restaurant 物件實例，並存入資料庫
+      name,
+      tel,
+      courseDescription,
+      teachingStyle
+    })
+      .then(() => {
+        req.flash('success_messages', 'tutor was successfully created') // 在畫面顯示成功提示
+        res.redirect('/admin/tutors') //新增完成後導回後台首頁
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth')
 
-router.get('/tutors', authenticatedAdmin, adminController.getTutors)
+router.get('/tutors/create', adminController.createTutor) 
+router.get('/tutors', adminController.getTutors)
+router.post('/tutors', adminController.postTutor)
 router.use('/', (req, res) => res.redirect('/admin/tutors'))
 module.exports = router
