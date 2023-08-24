c6229c3bfb077323d63e72f753df4c6b1ffdb47c
LAI <77723979+LAI-Recycle@users.noreply.github.com>
2023-08-24 23:53:07 +0800

    feat: modify admin tutors page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,12 @@
+const { Tutor } = require('../models')
+
 const adminController = {
   getTutors: (req, res) => {
-    return res.render('admin/tutors')
+    Tutor.findAll({
+      raw: true
+    })
+    .then(tutors => res.render('admin/tutors', { tutors }))
+    .catch(err => next(err))
   }
 }
 module.exports = adminController
